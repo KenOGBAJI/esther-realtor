1ea787f825428aa2007e536d366837eb91370807
Ogbaji Ignatius <Ogbaji.I@bullseye.local>
2023-06-27 18:10:02 +0100

    complete the listitem component

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -204,7 +204,7 @@ export default function CreateListing() {
              type="number" 
              id="bedrooms" 
              value={bedrooms} 
-             onChange={onChange}   min="1" max="50" 
+             onChange={onChange} min="1" max="50" 
              required
              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
             />
@@ -215,7 +215,7 @@ export default function CreateListing() {
              type="number" 
              id="bathrooms" 
              value={bathrooms} 
-             onChange={onChange}   min="1" max="50" 
+             onChange={onChange} min="1" max="50" 
              required
              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
             />
@@ -374,7 +374,7 @@ export default function CreateListing() {
                    />
                  {type === "rent" && (
                    <div className="">
-                     <p className="text-md w-full whitespace-nowrap"># / Month</p>
+                     <p className="text-md w-full whitespace-nowrap">₦ / Month</p>
                    </div>
                  )}
                  </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,6 @@ import { toast } from 'react-toastify';
 import { db } from '../firebase';
 import { FcHome } from 'react-icons/fc';
 import ListingItem from '../components/ListingItem';
-// import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const auth = getAuth();
@@ -126,8 +125,8 @@ export default function Profile() {
       <div className="max-w-6xl px-3 mt-6 mx-auto">
         {!loading && listings.length > 0 && (
           <>
-            <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
-            <ul>
+            <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
               {listings.map((listing) => (
                 <ListingItem 
                   key={listing.id} 
