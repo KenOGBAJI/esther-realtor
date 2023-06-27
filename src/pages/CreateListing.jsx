b2ea68a3c59880d110fc1f20cfb5269a7d73edb6
Ogbaji Ignatius <Ogbaji.I@bullseye.local>
2023-06-27 09:46:51 +0100

    create My Listings to the profile pageaand create the Listing Item component

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -142,6 +142,7 @@ export default function CreateListing() {
         imgUrls,
         geoLocation,
         timestamp: serverTimestamp(),
+        userRef: auth.currentUser.uid,
       };
       delete formDataCopy.images;
       !formDataCopy.offer && delete formDataCopy.discountedPrice;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,18 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { doc, updateDoc } from 'firebase/firestore';
-import { useState } from 'react'
+import { collection, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../firebase';
 import { FcHome } from 'react-icons/fc';
+import ListingItem from '../components/ListingItem';
 // import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -51,6 +53,25 @@ export default function Profile() {
       toast.error("Could not update the profile detail")
     }
   };
+
+  useEffect(() =>{
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q =query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp", "desc"));
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings()
+  }, [auth.currentUser.uid]);
+
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -93,14 +114,33 @@ export default function Profile() {
               <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>Sign out</p>
             </div>
           </form>
-              <button type='submit' className='w-full mt-4 bg-cyan-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
-                <Link to="/create-listing" className='flex justify-center items-center'>
-                <FcHome  className='mr-2 text-4xl bg-red-200 rounded-full p-1 border-2'/>
-                Sell or rent your home
+             <button type='submit' className='w-full mt-4 bg-cyan-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
+                <Link to="/create-listing" 
+                   className='flex justify-center items-center'>
+                  <FcHome  className='mr-2 text-4xl bg-red-200 rounded-full p-1 border-2'/>
+                   Sell or rent your home
                 </Link>
               </button>
         </div>
       </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem 
+                  key={listing.id} 
+                  id={listing.id} 
+                  listing={listing.data} 
+                />
+               ))}
+            </ul>
+          
+          </>
+
+        )}
+      </div>
     </>
   )
 }
